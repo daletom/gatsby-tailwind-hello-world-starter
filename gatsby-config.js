e007139af0260ef1bdcb6a7177ffd50be90812f7
daletom <tom@imgix.com>
2021-02-26 00:56:42 -0800

    add sitemetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,13 @@
  */
 
 module.exports = {
+  siteMetadata: {
+    title: `Gatsby WordPress Example`,
+    description: `An example to show how to source data from WordPress.`,
+    author: `tomdale`,
+    url: `https://wptogatsby.letsgoheadless.com`,
+    image: `https://images.unsplash.com/photo-1534796636912-3b95b3ab5986?blend64=aHR0cHM6Ly9kZW1vcy5pbWdpeC5uZXQvZGFyay1lbGxpcHNlLWdyYWRpZW50LmFpP2ZtPXBuZyZhcj0yOjE&blend-mode=multiply&blend-size=inherit&w=900&fit=crop&ar=3%3A1&txt64=RmFyIEZhciBBd2F5IE5ld3M&txt-color=white&txt-align=middle%2Ccenter&txt-fit=max&txt-size=72&auto=format,compress`,
+  },
   /* Your site config here */
   plugins: [
     {
